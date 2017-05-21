531fe139fe8859879f1cc9df97e99b13d3f93021
johnotu <johnne01@gmail.com>
2017-05-21 19:05:33 +0000

    Corrected User model location, Edited schema
    Replaced user in models with mistakenly created user content for routes.
    Edited user schema to include username, secret question and answer.
    Added more user actions including getUserByUsername, addUser and
    comparePassword.
    Changes to be committed:
    	copied:     routes/users.js -> models/user.js
    	modified:   routes/users.js

---
+++ models/user.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const config = require('../config/database');
+
+const UserSchema = mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    phoneNumber: {
+        type: String,
+        required: true
+    },
+    secretQuestion: {
+        type: String,
+        required: true
+    },
+    secretAnswer: {
+        type: String,
+        required: true
+    }
+});
+
+const User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.getUserById = function(id, callback){
+    User.findById(id, callback);
+}
+
+module.exports.getUserByUsername = function(username, callback){
+    const query = {username: username};
+    User.findOne(query, callback);
+}
+
+module.exports.addUser = function(newUser, callback){
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if(err) throw err;
+            newUser.password = hash;
+            newUser.save(callback);
+        })
+    })
+}
+
+module.exports.comparePassword = function(inputedPassword, hash, callback){
+    bcrypt.compare(inputedPassword, hash, (err, isMatch) => {
+        if(err) throw err;
+        callback(null, isMatch);
+    })
+}
