877a35bc883df4c73727a864de9c0a974b55ca4f
johnotu <johnne01@gmail.com>
2017-05-24 14:40:34 +0000

    Added more api routes
    Added /api/ routes for live Flutterwave Validation of account number,
    Tokenization of card details and Card enquiry.
    Changes to be committed:
    	modified:   routes/api.js

--- routes/api.js
+++ routes/api.js
@@ -5,9 +5,8 @@ const unirest = require('unirest');
 const BASE_URL = 'https://moneywave.herokuapp.com/';
 
 //GET list of banks in KEnya, Nigeria and Ghana
-router.get('/banks', (req, res, next) => {
-    /* FW Live retrieve bak acounts
-    let url = BASE_URL + 'banks?country=' + req.query.country;
+router.post('/banks', (req, res, next) => {
+    let url = BASE_URL + 'banks?country=' + req.body.country;
 
     unirest.post(url)
         .headers({'content-type': 'application/json'})
@@ -15,61 +14,75 @@ router.get('/banks', (req, res, next) => {
             res.json({result: response});
             console.log('Bank list fetched and delivered');
         })
-    */
-    // Mock data from JSON file
-    switch(req.query.country){
-        case "NG":
-            res.json({ result: banks.NG });
-            break;
-        case "KE":
-            res.json({ result: banks.KE });
-            break;
-        case "GH":
-            res.json({ result: banks.GH });
-            break;
-        default:
-            res.json({ result: 'Country not found' });
-            
-    }
 });
 
 //Validate Bank Account Numbers
-router.post('/account/resolve', (req, res, next) => {
-    let params = {
+router.post('/account/validate', (req, res, next) => {
+    let params = ({
         account_number: req.body.account_number,
         bank_code: req.body.bank_code
-    };
-    /* FW Live account resolution
-    let url = BASE_URL + 'v1/resolve/account';
-    let token = authenticateFW();
-    unirest.post(url)
-        .headers({'content-type': 'application/json', 'authorization': token})
-        .send(params)
-        .end((response) => {
-            res.json({result: response.body});
-            console.log('Account validation fetched and delivered');
-        })
-    */
-    let mockAccountHolders = ["Emmanuela Kyeta", "Francis Ngugi", "Felix Wathira", "Nguli Masheti", "Nareti Wambugu", "Kelechi Obi", "Mary Otu", "Haruna Adebayo", "Simeon Olugbenga", "Imaobong Eyo", "Sarah Agbanu", "Kwame Bimpong", "Jason Smith", "Nana Boateng", "Amma Boachye"]
-    res.json({result: {
-    "status": "success",
-    "data": {
-          "account_name": mockAccountHolders[Math.floor(Math.random() * mockAccountHolders.length)]
-    }
-}})
-});
-
-function authenticateFW(){
-    /* FW Live authentication
-    let url = BASE_URL + 'v1/merchant/verify';
-    unirest.post(url)
+    });
+    let urlResolve = BASE_URL + 'v1/resolve/account';
+    let urlVerify = BASE_URL + 'v1/merchant/verify';
+    console.log('params ' + params);
+    unirest.post(urlVerify)
         .headers({'content-type': 'application/json'})
         .send({'apiKey': process.env.FW_API_KEY, 'secret': process.env.FW_SECRET})
         .end((response) => {
-            return response.body.token;
+            let token = response.body.token;
+            unirest.post(urlResolve)
+                .headers({'content-type': 'application/json', 'authorization': token})
+                .send(params)
+                .end((response) => {
+                    res.json({result: response.body});
+                    console.log('Account validation fetched and delivered');
+                })
         })
-    */
-    return process.env.AUTH_TOKEN;
-}
+});
+
+//Tokenize Card
+router.post('/card/tokenize', (req, res, next) => {
+    let params = {
+        card_no: req.body.card_no,
+        cvv: req.body.cvv,
+        expiry_year: req.body.expiry_year, //format 2018
+        expiry_month: req.body.expiry_month //format 09
+    };
+    let urlVerify = BASE_URL + 'v1/merchant/verify';
+    let urlTokenize = BASE_URL + 'v1/transfer/charge/tokenize/card';
+    unirest.post(urlVerify)
+        .headers({'content-type': 'application/json'})
+        .send({'apiKey': process.env.FW_API_KEY, 'secret': process.env.FW_SECRET})
+        .end((response) => {
+            let token = response.body.token;
+            unirest.post(urlTokenize)
+                .headers({'content-type': 'application/json', 'authorization': token})
+                .send(params)
+                .end((response) => {
+                    res.json({result: response.body});
+                    console.log('Card data tokenized');
+                })
+        })
+})
+
+//Card Enquiry
+router.post('/card/enquiry', (req, res, next) => {
+    let urlVerify = BASE_URL + 'v1/merchant/verify';
+    let urlEnquiry = BASE_URL + 'v1/user/card/check';
+    let params = { cardNumber: req.body.cardNumber };
+    unirest.post(urlVerify)
+        .headers({'content-type': 'application/json'})
+        .send({'apiKey': process.env.FW_API_KEY, 'secret': process.env.FW_SECRET})
+        .end((response) => {
+            let token = response.body.token;
+            unirest.post(urlEnquiry)
+                .headers({'content-type': 'application/json', 'authorization': token})
+                .send(params)
+                .end((response) => {
+                    res.json({result: response.body});
+                    console.log('Card enquiry done');
+                })
+        })
+})
 
 module.exports = router;
