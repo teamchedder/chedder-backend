b97f27a167db4780fd250287f7b968d55097c040
johnotu <johnne01@gmail.com>
2017-06-01 20:16:54 +0000

    Linked DB with new details since switch from dev to master

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-    database: 'mongodb://127.0.0.1:27017/chedder',
+    database: 'mongodb://<chedder>:<chedder>@ds161001.mlab.com:61001/chedder',
     secret: process.env.DB_SECRET
 }
